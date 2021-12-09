e21b9ade3956de16c2cfabf23cbc1a505832ef46
seondongpyo <seondongpyo@gmail.com>
2021-12-09 22:02:19 +0900

    create a React Native sandbox app by react-native-cli

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.main}>
+      <Text>This is sandbox!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
