4aff14318dc2167b7c0e2ee13084e5c956bd7d39
seondongpyo <seondongpyo@gmail.com>
2021-12-10 11:07:01 +0900

    react-native-vector-icons 적용

--- src/navigation/tab/TabNavigator.js
+++ src/navigation/tab/TabNavigator.js
@@ -3,15 +3,35 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeTabScreen from './HomeTabScreen';
 import StackNavigator from '../stack/StackNavigator';
 import TodoTabScreen from './TodoTabScreen';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const TabNavigator = () => {
   const Tab = createBottomTabNavigator();
 
+  const TabBarIcon = (focused, name) => {
+    let iconName;
+    let iconSize = focused ? 30 : 20;
+
+    if (name === 'Home') {
+      iconName = 'home';
+    } else if (name === 'Todo') {
+      iconName = 'check-square';
+    } else if (name === 'Sandbox') {
+      iconName = 'inbox';
+    }
+
+    return <Icon name={iconName} size={iconSize} />;
+  };
+
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerTitleAlign: 'center',
-      }}>
+        // tabBarActiveBackgroundColor: 'skyblue',
+        // tabBarActiveTintColor: 'blue',
+        tabBarIcon: ({ focused }) => TabBarIcon(focused, route.name),
+        tabBarLabelPosition: 'below-icon',
+      })}>
       <Tab.Screen name="Home" component={HomeTabScreen} />
       <Tab.Screen name="Todo" component={TodoTabScreen} />
       <Tab.Screen name="Sandbox" component={StackNavigator} />
