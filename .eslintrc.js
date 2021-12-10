016413fc01db81ca7e506f4eb45788dd7383916a
seondongpyo <seondongpyo@gmail.com>
2021-12-10 11:08:52 +0900

    줄바꿈을 LF로 통일

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
+	'prettier/prettier': ['error', { endOfLine: 'auto'} ],
 };
